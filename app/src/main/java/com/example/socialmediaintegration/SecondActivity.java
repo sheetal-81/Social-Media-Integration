cd36ed3a23edd6c459a5c6239d65c31302ba1717
Sheetal123-gupta <134307985+sheetal-81@users.noreply.github.com>
2024-01-20 10:38:24 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/socialmediaintegration/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.socialmediaintegration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+    }
+}
